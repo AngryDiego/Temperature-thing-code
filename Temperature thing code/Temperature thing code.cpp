f69fa9dcd11028a91b874ea8d5bd1db81c8adacb
DiegoVS <Diegoanchukov@gmail.com>
2025-04-26 13:57:32 +0200

    Rework of code

--- Temperature thing code/Temperature thing code.cpp
+++ Temperature thing code/Temperature thing code.cpp
@@ -1,29 +1,36 @@
 #include <iostream>
 using namespace std;
 
+string HighestTemp(const int SIZE, const string DAYS[], double TEMP[]) {
+    double bigest_temp = 0;
+    int h_t_d = 0; // Highest Temprature Day
+
+    for (int i = 0; i < SIZE; i++) {
+        if (TEMP[i] > bigest_temp) {
+            bigest_temp = TEMP[i];
+            h_t_d = i;
+        }
+    }
+
+    return DAYS[h_t_d];
+}
+
+double AverageTemp(const int SIZE, double TEMP[]) {
+    double average_temp = 0;
+    for (int i = 0; i < SIZE; i++) {
+        average_temp += TEMP[i];
+    }
+    average_temp /= SIZE;
+
+    return average_temp;
+}
+
 int main()
 {
     const int WEEK_ARR = 7;
     string days[WEEK_ARR] = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
     double arr[WEEK_ARR] = {15.6, 12.8, 17.9, 14.2, 16.6, 18.1, 17.2};
 
-    double bigest_temp = 0;
-    double average_temp = 0;
-    int h_t_d = 0; // Highest Temprature Day
-
-    for (int i = 0; i < WEEK_ARR; i++) {
-        if (arr[i] > bigest_temp) {
-            bigest_temp = arr[i];
-            h_t_d = i;
-        }
-    }
-    cout << bigest_temp << endl << days[h_t_d] << endl;
-
-    for (int i = 0; i < WEEK_ARR; i++) {
-        average_temp += arr[i];
-    }
-    average_temp /= WEEK_ARR;
-
-    cout << average_temp << " Average temprature within week";
-
+    cout << "Highest temperature day: " << HighestTemp(WEEK_ARR, days, arr) << endl;
+    cout << "Average temperature within week: " << AverageTemp(WEEK_ARR, arr) << endl;
 }
